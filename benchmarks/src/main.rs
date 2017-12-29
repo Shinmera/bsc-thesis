7c0062ccc190dc53d8da1dc8585f6a5da633d611
Shinmera <shinmera@tymoon.eu>
2017-12-29 15:31:23 +0100

    Fix join operator to actually work.

--- benchmarks/src/main.rs
+++ benchmarks/src/main.rs
@@ -1,4 +1,3 @@
-extern crate either;
 extern crate timely;
 mod operators;
 
@@ -6,17 +5,16 @@ use operators::RollingCount;
 use operators::EpochWindow;
 use operators::Join;
 use timely::dataflow::operators::{Input, Probe, Inspect};
-use either::{Either};
 
 fn main() {
     timely::execute_from_args(std::env::args(), move |worker| {
-        let data = vec![
+        let mut data1 = vec![
             vec![('a', 0), ('b', 0)],
             vec![('a', 1), ('b', 1), ('c', 1)],
             vec![('a', 2)],
             vec![('a', 3), ('b', 3), ('c', 3)],
         ];
-        let data2 = vec![
+        let mut data2 = vec![
             vec![('a', 2), ('b', 1)],
             vec![('a', 1), ('b', 1), ('c', 1)],
             vec![('a', 2)],
@@ -24,44 +22,26 @@ fn main() {
         ];
 
         let index = worker.index();
-        let mut input = timely::dataflow::InputHandle::new();
-        let mut input2 = timely::dataflow::InputHandle::new();
 
-        // let _probe = worker.dataflow(|scope| {
-        //     scope.input_from(&mut input)
-        //         .rolling_count(|&(name, score)| (name, score))
-        //         .inspect(move |&(name, score)| println!("worker {}:\t {}: {}", index, name, score))
-        //         .probe()
-        // });
-
-        // let _probe = worker.dataflow(|scope| {
-        //     scope.input_from(&mut input)
-        //         .epoch_window(2, 2)
-        //         .inspect(move |ref v| println!("worker {}:\t {:?}", index, v))
-        //         .probe()
-        // });
-
-        let stream2 = worker.dataflow(|scope| {
-            scope.input_from(&mut input2)
-        });
-
-        let _probe = worker.dataflow(|scope| {
-            scope.input_from(&mut input)
+        let (mut input1, mut input2, _probe) = worker.dataflow(move |scope| {
+            let (input1, stream1) = scope.new_input();
+            let (input2, stream2) = scope.new_input();
+            let probe = stream1
                 .join(&stream2,
-                      |dat| dat.either(|&(k, s)| s, |&(k, s)| s),
-                      |&(name1, score1), &(name2, score2)| score1+score2)
+                      |&(name, _)| name, |&(name, _)| name,
+                      |&(name, s1), &(_, s2)| (name, s1+s2))
                 .inspect(move |&(name, score)| println!("worker {}:\t {}: {}", index, name, score))
-                .probe()
+                .probe();
+            (input1, input2, probe)
         });
 
-        let mut current_epoch = 0;
-        for epoch in 0..data.len() {
-            input.advance_to(epoch);
-            for dat in data[epoch]{
-                input.send(dat);
+        for epoch in 0..data1.len() {
+            input1.advance_to(epoch);
+            for dat in data1.pop().unwrap(){
+                input1.send(dat);
             }
             input2.advance_to(epoch);
-            for dat in data2[epoch]{
+            for dat in data2.pop().unwrap(){
                 input2.send(dat);
             }
         }
--- benchmarks/src/operators/join.rs
+++ benchmarks/src/operators/join.rs
@@ -5,21 +5,22 @@ use timely::Data;
 use timely::dataflow::channels::pact::Pipeline;
 use timely::dataflow::operators::generic::Binary;
 use timely::dataflow::{Stream, Scope};
-use either::{Either, Left, Right};
 
 pub trait Join<G: Scope, D1: Data> {
-    fn join<H, D2, D3, K, J>(&self, stream: &Stream<G, D2>, key_extractor: K, joiner: J) -> Stream<G, D3>
+    fn join<H, D2, D3, K1, K2, J>(&self, stream: &Stream<G, D2>, key_1: K1, key_2: K2, joiner: J) -> Stream<G, D3>
         where H: Hash+Eq+Data+Clone,
               D2: Data, D3: Data,
-              K: Fn(Either<&D1,&D2>)->H+'static,
+              K1: Fn(&D1)->H+'static,
+              K2: Fn(&D2)->H+'static,
               J: Fn(&D1, &D2)->D3+'static;
 }
 
 impl<G: Scope, D1: Data> Join<G, D1> for Stream<G, D1> {
-    fn join<H, D2, D3, K, J>(&self, stream: &Stream<G, D2>, key_extractor: K, joiner: J) -> Stream<G, D3>
+    fn join<H, D2, D3, K1, K2, J>(&self, stream: &Stream<G, D2>, key_1: K1, key_2: K2, joiner: J) -> Stream<G, D3>
         where H: Hash+Eq+Data+Clone,
               D2: Data, D3: Data,
-              K: Fn(Either<&D1,&D2>)->H+'static,
+              K1: Fn(&D1)->H+'static,
+              K2: Fn(&D2)->H+'static,
               J: Fn(&D1, &D2)->D3+'static{
         let mut epoch1 = HashMap::new();
         let mut epoch2 = HashMap::new();
@@ -28,7 +29,7 @@ impl<G: Scope, D1: Data> Join<G, D1> for Stream<G, D1> {
             input1.for_each(|time, data|{
                 let epoch = epoch1.entry(time.clone()).or_insert(HashMap::new());
                 while let Some(dat) = data.pop(){
-                    let key = key_extractor(Left(&dat));
+                    let key = key_1(&dat);
                     let datavec = epoch.entry(key).or_insert(Vec::new());
                     datavec.push(dat);
                 }
@@ -38,7 +39,7 @@ impl<G: Scope, D1: Data> Join<G, D1> for Stream<G, D1> {
             input2.for_each(|time, data|{
                 let epoch = epoch2.entry(time.clone()).or_insert(HashMap::new());
                 while let Some(dat) = data.pop(){
-                    let key = key_extractor(Right(&dat));
+                    let key = key_2(&dat);
                     let datavec = epoch.entry(key).or_insert(Vec::new());
                     datavec.push(dat);
                 }
@@ -46,16 +47,17 @@ impl<G: Scope, D1: Data> Join<G, D1> for Stream<G, D1> {
             });
             
             notificator.for_each(|time, _, _|{
-                let k1 = epoch1.get(&time).unwrap_or(&HashMap::new());
-                let k2 = epoch2.get(&time).unwrap_or(&HashMap::new());
-                let out = output.session(&time);
-                for (key, data1) in k1{
-                    k2.get(key).and_then(|data2| {
-                        for i in 0..min(data1.len(), data2.len()){
-                            out.give(joiner(&data1[i], &data2[i]));
+                if let Some(k1) = epoch1.get(&time) {
+                    if let Some(k2) = epoch2.get(&time) {
+                        let mut out = output.session(&time);
+                        for (key, data1) in k1{
+                            if let Some(data2) = k2.get(key) {
+                                for i in 0..min(data1.len(), data2.len()){
+                                    out.give(joiner(&data1[i], &data2[i]));
+                                }
+                            }
                         }
-                        None
-                    });
+                    }
                 }
             });
         })
