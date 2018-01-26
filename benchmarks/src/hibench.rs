7c40cb0bcb02cf08b54772bfe6b28e98ad340237
Shinmera <shinmera@tymoon.eu>
2018-01-26 13:06:20 +0100

    Comment out for now

--- benchmarks/src/hibench.rs
+++ benchmarks/src/hibench.rs
@@ -22,56 +22,56 @@ impl<A: Allocate> TestImpl<A> for Identity {
     }
 }
 
-struct Repartition {}
-impl<A: Allocate> Test<A> for Repartition{}
-impl<A: Allocate> TestImpl<A> for Repartition {
-    type D = String;
-    type T = usize;
+// struct Repartition {}
+// impl<A: Allocate> Test<A> for Repartition{}
+// impl<A: Allocate> TestImpl<A> for Repartition {
+//     type D = String;
+//     type T = usize;
     
-    fn name(&self) -> str { "Repartition" }
+//     fn name(&self) -> str { "Repartition" }
     
-    fn construct_dataflow(&self, scope: &mut Child<Self::G, Self::T>) -> (Stream<Self::G, Self::D>, [Box<Handle<Self::T, Self::D>>]) {
-        let (input, stream) = scope.new_input();
-        let stream = stream
-            .exchange(|&x| x);
-        (vec![input], stream)
-    }
-}
+//     fn construct_dataflow(&self, scope: &mut Child<Self::G, Self::T>) -> (Stream<Self::G, Self::D>, [Box<Handle<Self::T, Self::D>>]) {
+//         let (input, stream) = scope.new_input();
+//         let stream = stream
+//             .exchange(|&x| x);
+//         (vec![input], stream)
+//     }
+// }
 
-struct Wordcount {}
-impl<A: Allocate> Test<A> for Wordcount{}
-impl<A: Allocate> TestImpl<A> for Wordcount {
-    type D = String;
-    type T = usize;
+// struct Wordcount {}
+// impl<A: Allocate> Test<A> for Wordcount{}
+// impl<A: Allocate> TestImpl<A> for Wordcount {
+//     type D = String;
+//     type T = usize;
     
-    fn name(&self) -> str { "Wordcount" }
+//     fn name(&self) -> str { "Wordcount" }
 
-    fn construct_dataflow(&self, scope: &mut Child<Self::G, Self::T>) -> (Stream<Self::G, Self::D>, [Box<Handle<Self::T, Self::D>>]) {
-        let (input, stream) = scope.new_input();
-        let stream = stream
-            .flat_map(|text| text.split_whitespace())
-            .exchange(|word| word)
-            .rolling_count(|word| (word, 1));
-        (vec![input], stream)
-    }
-}
+//     fn construct_dataflow(&self, scope: &mut Child<Self::G, Self::T>) -> (Stream<Self::G, Self::D>, [Box<Handle<Self::T, Self::D>>]) {
+//         let (input, stream) = scope.new_input();
+//         let stream = stream
+//             .flat_map(|text| text.split_whitespace())
+//             .exchange(|word| word)
+//             .rolling_count(|word| (word, 1));
+//         (vec![input], stream)
+//     }
+// }
 
-struct Fixwindow {}
-impl<A: Allocate> Test<A> for Fixwindow{}
-impl<A: Allocate> TestImpl<A> for Fixwindow {
-    type D = String;
-    type T = usize;
+// struct Fixwindow {}
+// impl<A: Allocate> Test<A> for Fixwindow{}
+// impl<A: Allocate> TestImpl<A> for Fixwindow {
+//     type D = String;
+//     type T = usize;
     
-    fn name(&self) -> str { "Fixwindow" }
+//     fn name(&self) -> str { "Fixwindow" }
 
-    fn construct_dataflow(&self, scope: &mut Child<Self::G, Self::T>) -> (Stream<Self::G, Self::D>, [Box<Handle<Self::T, Self::D>>]) {
-        let (input, stream) = scope.new_input();
-        let stream = stream
-            .epoch_window(1, 1)
-            .map(|data| data.iter().sum());
-        (vec![input], stream)
-    }
-}
+//     fn construct_dataflow(&self, scope: &mut Child<Self::G, Self::T>) -> (Stream<Self::G, Self::D>, [Box<Handle<Self::T, Self::D>>]) {
+//         let (input, stream) = scope.new_input();
+//         let stream = stream
+//             .epoch_window(1, 1)
+//             .map(|data| data.iter().sum());
+//         (vec![input], stream)
+//     }
+// }
 
 pub fn hibench<A: Allocate>() -> [Box<Test<A>>]{
     vec![Box::new(Identity{})]
