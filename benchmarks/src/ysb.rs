79309d10f06e6a0dfcd4bae522b7b23debe648ed
Shinmera <shinmera@tymoon.eu>
2018-02-14 15:51:05 +0100

    Work some more on the YSB benchmark data flow. Lifetimes are once again causing me headaches though, and I'll probably need some help figuring out how to placate Rust.

--- benchmarks/src/ysb.rs
+++ benchmarks/src/ysb.rs
@@ -5,7 +5,7 @@ use timely::dataflow::operators::input::Handle;
 use timely::dataflow::scopes::{Root, Child};
 use timely::dataflow::{Stream};
 use timely_communication::allocator::Generic;
-use operators::{EpochWindow, Join};
+use operators::{EpochWindow};
 use test::Test;
 use test::TestImpl;
 
@@ -22,15 +22,10 @@ struct RawData {
 
 unsafe_abomonate!(RawData : user_id, page_id, ad_id, ad_type, event_type, event_time, ip_address);
 
-#[derive(Eq, PartialEq, Clone)]
-struct Campaign {
-    ad_id: String,
-    campaign_id: String,
+struct YSB {
+    campaign_map: HashMap<String, String>
 }
 
-unsafe_abomonate!(Campaign : ad_id, campaign_id);
-
-struct YSB {}
 impl TestImpl for YSB {
     type D = RawData;
     type DO = Vec<(String, usize)>;
@@ -40,14 +35,27 @@ impl TestImpl for YSB {
 
     fn initial_epoch(&self) -> Self::T { 0 }
 
+    fn prepare_data(&self, _index: usize) -> Result<bool, String> {
+        // FIXME: fill campaign_map
+        Ok(false)
+    }
+
     fn construct_dataflow<'scope>(&self, scope: &mut Child<'scope, Root<Generic>, Self::T>) -> (Stream<Child<'scope, Root<Generic>, Self::T>, Self::DO>, Vec<Handle<Self::T, Self::D>>) {
         let (input, stream) = scope.new_input();
-        let (table_input, table_stream) = scope.new_input();
         let stream = stream
+            // Filter to view event_type events.
             .filter(|x: &RawData| x.event_type == "view")
+            // Transform/Project to ad_id and event_time.
             .map(|x| (x.ad_id, x.event_time))
-            .join(&table_stream, |&(ad_id, _)| ad_id, |y: &Campaign| y.ad_id, |x, y| y.campaign_id)
+            // Join the ad_id into the campaign_id through a table lookup.
+            .map(|(ad_id, _)|
+                 match self.campaign_map.get(&ad_id){
+                     Some(id) => id.clone(),
+                     None => String::from("UNKNOWN AD")
+                 })
+            // Aggregate to a window based on timed epochs.
             .epoch_window(10, 10)
+            // Count each campaign in the window and return as tuples of id + count.
             .map(|x| {
                 let counts = HashMap::new();
                 for campaign_id in x {
@@ -55,10 +63,10 @@ impl TestImpl for YSB {
                 }
                 counts.drain().collect::<Vec<_>>()
             });
-        (stream, vec![input, table_input])
+        (stream, vec![input])
     }
 }
 
 pub fn ysb() -> Vec<Box<Test>>{
-    vec![Box::new(YSB{})]
+    vec![Box::new(YSB{campaign_map: HashMap::new()})]
 }
