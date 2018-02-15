db8a0a100974b251586b17f95310d87d6cc460c6
Shinmera <shinmera@tymoon.eu>
2018-02-15 15:38:19 +0100

    Add some documentation about the test framework.

--- benchmarks/src/test.rs
+++ benchmarks/src/test.rs
@@ -10,6 +10,8 @@ use timely::progress::nested::product::Product;
 use timely::progress::timestamp::RootTimestamp;
 use std::ops::Add;
 
+/// Simple trait for incrementable objects.
+/// This is used to automatically advance the timestamp.
 pub trait Inc: Copy {
     fn next(&mut self) -> Self;
 }
@@ -28,24 +30,50 @@ impl Inc for isize { fn next(&mut self) -> Self {self.add(1)} }
 impl Inc for f32 { fn next(&mut self) -> Self {self.add(1.0)} }
 impl Inc for f64 { fn next(&mut self) -> Self {self.add(1.0)} }
 
+/// This trait exposes the implementation details of a benchmark.
+///
+/// The idea is that it encapsulates common functionality between
+/// the tests and avoids duplicating code. It also presents a
+/// simple framework for implementing a benchmark.
 pub trait TestImpl : Sync+Send{
     type D: Data;
     type DO: Data;
     type T: Timestamp+Inc;
-    
+
+    /// The name of the test as a human readable string.
     fn name(&self) -> &str;
     
+    /// This function is responsible for constructing the data flow
+    /// used for the computation, returning the last node of the
+    /// graph and a vector of input handles.
+    ///
+    /// The input argument should be the object received by the
+    /// function passed to worker.dataflow()
     fn construct_dataflow<'scope>(&self, &mut Child<'scope, Root<Generic>, Self::T>) -> (Stream<Child<'scope, Root<Generic>, Self::T>, Self::DO>, Vec<Handle<Self::T, Self::D>>);
 
+    /// This function is called at the beginning of a test run and
+    /// is responsible for opening input streams and preparing data.
+    /// The index is the current worker's index, which is used to
+    /// decide whether this should feed the dataflow at all.
+    ///
+    /// If this worker should not feed the dataflow, the result
+    /// should be Ok(false), otherwise Ok(true). Err(..) can be used
+    /// to report on unexpected failures in preparing or parsing the
+    /// data.
     fn prepare_data(&self, _index: usize) -> Result<bool, &str> {
         Ok(false)
     }
 
+    /// Generates a single run of data for an epoch. If there is no
+    /// more data available (and the test is thus over), the function
+    /// should return None.
     fn generate_data(&self) -> Option<Vec<Self::D>> {
         println!("Warning: {} does not implement a data generator.", self.name());
         None
     }
 
+    /// A shorthand function to test whether the frontier has reached
+    /// all the inputs yet.
     fn frontier_behind(&self, probe: &ProbeHandle<Product<RootTimestamp, Self::T>>, inputs: &Vec<Handle<Self::T, Self::D>>) -> bool{
         for input in inputs {
             if probe.less_than(input.time()) {
@@ -55,8 +83,26 @@ pub trait TestImpl : Sync+Send{
         return false;
     }
 
+    /// This function will return the starting epoch timestamp.
     fn initial_epoch(&self) -> Self::T;
 
+    /// This function handles the actual running of the test.
+    ///
+    /// It first calls prepare_data to prepare the local test.
+    /// It then runs the dataflow construction, coercing it to
+    /// a list of inputs and a probe. It then starts to loop.
+    /// If the current worker feeds data, it will call
+    /// generate_data and send it on to each of the inputs.
+    /// It then advances the epoch and steps the worker until
+    /// the frontier is no longer behind.
+    /// Once the loop exits, due to no longer having any new
+    /// input, all inputs are closed and Ok(()) is returned.
+    ///
+    /// This function is the primary raison d'être of this
+    /// framework, as this behaviour is the same across each
+    /// possible test we might to run; you open some streams,
+    /// construct the data flow, loop to feed data and advance
+    /// the workers as needed.
     fn run(&self, worker: &mut Root<Generic>) -> Result<(), &str>{
         let provides_input = self.prepare_data(worker.index())?;
         let (probe, mut inputs) = worker.dataflow(|scope|{
@@ -90,6 +136,11 @@ pub trait TestImpl : Sync+Send{
     }
 }
 
+/// This presents the public interface for a test.
+///
+/// The reason this exists is so that you can talk about a test
+/// without having to know the precise type of the test, or the
+/// TestImpl's trait types.
 pub trait Test : Sync+Send{
     fn name(&self) -> &str;
     fn run(&self, worker: &mut Root<Generic>) -> Result<(), &str>;
