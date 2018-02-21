c752f1fe32d0f4824d56c7e77d19111993197fae
Shinmera <shinmera@tymoon.eu>
2018-02-21 23:50:10 +0100

    Fix epoch window notification for out of order epochs.

--- benchmarks/src/operators/epochwindow.rs
+++ benchmarks/src/operators/epochwindow.rs
@@ -24,29 +24,32 @@ impl<G: Scope, D: Data> EpochWindow<G, D> for Stream<G, D> {
                     part.append(data.deref_mut());
                     // Remember this time for reconstruction of partial windows.
                     if !times.contains(time) {
+                        // FIXME: fix for out of order epoch arrival.
                         times.push_back(time.clone());
                     }
                 }
                 
-                if size <= times.len() && (times.len()-size) % slide == 0 {
-                    notificator.notify_at(cap);
-                }
+                notificator.notify_at(cap);
             });
             notificator.for_each(|cap, _, _| {
-                // Gather complete window from partial windows.
-                let mut window = Vec::new();
-                times.iter().take(size).for_each(|time|{
-                    if let Some(part) = window_parts.get(time){
-                        part.iter().for_each(|entry| window.push(entry.clone()));
-                    }
-                });
-                // Send out the completed window.
-                output.session(&cap).give_iterator(window.drain(..));
-                // Invalidate partial windows that fell out of the slide.
-                let count = min(slide, times.len());
-                times.drain(0..count).for_each(|time|{
-                    window_parts.remove(&time);
-                });
+                let pos = 1 + times.iter().position(|x| x == cap.time()).unwrap_or(size);
+                // Only send out data if this is on a complete window.
+                if size <= pos && (pos-size) % slide == 0 {
+                    // Gather complete window from partial windows.
+                    let mut window = Vec::new();
+                    times.iter().take(size).for_each(|time|{
+                        if let Some(part) = window_parts.get(time){
+                            part.iter().for_each(|entry| window.push(entry.clone()));
+                        }
+                    });
+                    // Send out the completed window.
+                    output.session(&cap).give_iterator(window.drain(..));
+                    // Invalidate partial windows that fell out of the slide.
+                    let count = min(slide, times.len());
+                    times.drain(0..count).for_each(|time|{
+                        window_parts.remove(&time);
+                    });
+                }
             });
         })
     }
