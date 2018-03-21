d20b837a8eeeb25f9a18f2cd89eaf42aef3827d1
Shinmera <shinmera@tymoon.eu>
2018-03-21 19:25:25 +0100

    This seems to compile, woa

--- benchmarks/src/endpoint.rs
+++ benchmarks/src/endpoint.rs
@@ -4,6 +4,7 @@ use std::fmt::Debug;
 use timely::dataflow::operators::capture::event::{Event, EventIterator, EventPusher};
 use timely::progress::timestamp::{Timestamp, RootTimestamp};
 use timely::progress::nested::product::Product;
+use timely::Data;
 
 pub struct Null();
 
@@ -47,51 +48,59 @@ impl<T: Timestamp, D> EventPusher<T, D> for Null {
     fn push(&mut self, _: Event<T, D>) {}
 }
 
-pub struct Console(Stdin, Stdout);
+pub struct Console<T: Timestamp, D>{
+    stdin: Stdin,
+    stdout: Stdout,
+    next: Option<Event<T, D>>,
+}
 
-impl Console {
+impl<T: Timestamp, D> Console<T, D> {
     pub fn new() -> Self {
-        Console(io::stdin(), io::stdout())
+        Console{stdin: io::stdin(), stdout: io::stdout(), next: None}
     }
 }
 
-impl<T: Timestamp, D> EventIterator<T, D> for Console where String: ToData<T, D>{
+impl<T: Timestamp, D> EventIterator<T, D> for Console<T, D> where String: ToData<T, D>{
     fn next(&mut self) -> Option<&Event<T, D>> {
-        let &mut Console(ref mut stdin, _) = self;
-        to_message(||{
+        let ref mut stdin = self.stdin;
+        self.next = to_message(||{
             let mut line = String::new();
             stdin.read_line(&mut line).ok()
                 .and_then(|_| line.to_data())
-        }).map(|m| &m)
+        });
+        self.next.as_ref()
     }
 }
 
-impl<T: Timestamp+Debug, D: Debug> EventPusher<T, D> for Console {
+impl<T: Timestamp+Debug, D: Debug> EventPusher<T, D> for Console<T, D> {
     fn push(&mut self, event: Event<T, D>) {
-        let &mut Console(_, ref mut stdout) = self;
+        let ref mut stdout = self.stdout;
         if let Event::Messages(t, d) = event {
-            stdout.write_fmt(format_args!("{:?} {:?}", t, d));
+            stdout.write_fmt(format_args!("{:?} {:?}", t, d)).unwrap();
         }
     }
 }
 
-pub struct FileInput(Lines<BufReader<File>>);
+pub struct FileInput<T: Timestamp, D>{
+    stream: Lines<BufReader<File>>,
+    next: Option<Event<T, D>>
+}
 
-impl FileInput {
+impl<T: Timestamp, D> FileInput<T, D> {
     pub fn new(f: File) -> Self {
-        FileInput(BufReader::new(f).lines())
+        FileInput{stream: BufReader::new(f).lines(), next: None}
     }
 }
 
-impl<T: Timestamp, D> EventIterator<T, D> for FileInput where String: ToData<T, D> {
+impl<T: Timestamp, D> EventIterator<T, D> for FileInput<T, D> where String: ToData<T, D> {
     fn next(&mut self) -> Option<&Event<T, D>> {
-        let &mut FileInput(ref mut reader) = self;
-
-        to_message(||{
-            reader.next()
+        let ref mut stream = self.stream;
+        self.next = to_message(||{
+            stream.next()
                 .and_then(|n| n.ok())
                 .and_then(|l| l.to_data())
-        }).map(|m| &m)
+        });
+        self.next.as_ref()
     }
 }
 
@@ -110,7 +119,7 @@ impl<T: Timestamp, D> EventIterator<T, D> for Source<T, D> {
     }
 }
 
-impl<T: Timestamp, D> Into<Source<T, D>> for File where String: ToData<T, D> {
+impl<T: Timestamp, D: Data> Into<Source<T, D>> for File where String: ToData<T, D> {
     fn into(self) -> Source<T, D> {
         Source::new(Box::new(FileInput::new(self)))
     }
