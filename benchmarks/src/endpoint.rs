751d174d7d92a9fa19d1c81391d0808e0b81af71
Shinmera <shinmera@tymoon.eu>
2018-03-22 10:09:17 +0100

    Implement config based conversion, kafka.

--- benchmarks/src/endpoint.rs
+++ benchmarks/src/endpoint.rs
@@ -1,10 +1,13 @@
-use std::io::{self, Write, Stdout, Stdin, Lines, BufReader, BufRead};
+use config::Config;
+use std::io::{self, Result, Error, ErrorKind, Write, Stdout, Stdin, Lines, BufReader, BufRead, BufWriter};
 use std::fs::File;
-use std::fmt::Debug;
+use std::fmt::Display;
 use timely::dataflow::operators::capture::event::{Event, EventIterator, EventPusher};
 use timely::progress::timestamp::{Timestamp, RootTimestamp};
 use timely::progress::nested::product::Product;
 use timely::Data;
+use kafkaesque::{self};
+use rdkafka::config::ClientConfig;
 
 pub struct Null();
 
@@ -72,16 +75,18 @@ impl<T: Timestamp, D> EventIterator<T, D> for Console<T, D> where String: ToData
     }
 }
 
-impl<T: Timestamp+Debug, D: Debug> EventPusher<T, D> for Console<T, D> {
+impl<T: Timestamp+Display, D: Display> EventPusher<T, D> for Console<T, D> {
     fn push(&mut self, event: Event<T, D>) {
         let ref mut stdout = self.stdout;
         if let Event::Messages(t, d) = event {
-            stdout.write_fmt(format_args!("{:?} {:?}", t, d)).unwrap();
+            for e in d {
+                stdout.write_fmt(format_args!("{} {}\n", t, e)).unwrap();
+            }
         }
     }
 }
 
-pub struct FileInput<T: Timestamp, D>{
+pub struct FileInput<T: Timestamp, D> {
     stream: Lines<BufReader<File>>,
     next: Option<Event<T, D>>
 }
@@ -104,6 +109,27 @@ impl<T: Timestamp, D> EventIterator<T, D> for FileInput<T, D> where String: ToDa
     }
 }
 
+pub struct FileOutput {
+    stream: BufWriter<File>
+}
+
+impl FileOutput {
+    pub fn new(f: File) -> Self {
+        FileOutput{stream: BufWriter::new(f)}
+    }
+}
+
+impl<T: Timestamp+Display, D: Display> EventPusher<T, D> for FileOutput {
+    fn push(&mut self, event: Event<T, D>) {
+        let ref mut stream = self.stream;
+        if let Event::Messages(t, d) = event {
+            for e in d {
+                stream.write_fmt(format_args!("{} {}\n", t, e)).unwrap();
+            }
+        }
+    }
+}
+
 pub struct Source<T, D>(Box<EventIterator<T, D>>);
 
 impl<T: Timestamp, D> Source<T, D> {
@@ -119,9 +145,48 @@ impl<T: Timestamp, D> EventIterator<T, D> for Source<T, D> {
     }
 }
 
+impl<T: Timestamp, D> Into<Source<T, D>> for () {
+    fn into(self) -> Source<T, D> {
+        Source::new(Box::new(Null::new()))
+    }
+}
+
+impl<T: Timestamp, D: Data> Into<Source<T, D>> for Stdin where String: ToData<T, D> {
+    fn into(self) -> Source<T, D> {
+        Source::new(Box::new(Console::new()))
+    }
+}
+
 impl<T: Timestamp, D: Data> Into<Source<T, D>> for File where String: ToData<T, D> {
     fn into(self) -> Source<T, D> {
         Source::new(Box::new(FileInput::new(self)))
+    }
+}
+
+impl<T: Timestamp+Display, D: Data+Display> Into<Result<Source<T, D>>> for Config
+where Stdin: Into<Source<T, D>>,
+      File: Into<Source<T, D>> {
+    fn into(self) -> Result<Source<T, D>> {
+        match self.get_or("input", "console").as_ref() {
+            "null" => {
+                Ok(().into())
+            },
+            "console" => {
+                Ok(io::stdin().into())
+            },
+            "file" => {
+                let file = File::open(self.get_or("input-file", "input.log"))?;
+                Ok(file.into())
+            },
+            "kafka" => {
+                let mut config = ClientConfig::new();
+                config
+                    .set("produce.offset.report", "true")
+                    .set("bootstrap.servers", &self.get_or("kafka-server", "localhost:9092"));
+                Ok(Source::new(Box::new(kafkaesque::EventConsumer::new(config, self.get_or("kafka-topic", "1")))))
+            },
+            _ => Err(Error::new(ErrorKind::Other, "Unknown output."))
+        }
     }
 }
 
@@ -143,5 +208,42 @@ impl<T: Timestamp, D> EventPusher<T, D> for Drain<T, D> {
 impl<T: Timestamp, D> Into<Drain<T, D>> for () {
     fn into(self) -> Drain<T, D> {
         Drain::new(Box::new(Null::new()))
+    }
+}
+
+impl<T: Timestamp+Display, D: Data+Display> Into<Drain<T, D>> for Stdout {
+    fn into(self) -> Drain<T, D> {
+        Drain::new(Box::new(Console::new()))
+    }
+}
+
+impl<T: Timestamp+Display, D: Data+Display> Into<Drain<T, D>> for File {
+    fn into(self) -> Drain<T, D> {
+        Drain::new(Box::new(FileOutput::new(self)))
+    }
+}
+
+impl<T: Timestamp+Display, D: Data+Display> Into<Result<Drain<T, D>>> for Config {
+    fn into(self) -> Result<Drain<T, D>> {
+        match self.get_or("output", "console").as_ref() {
+            "null" => {
+                Ok(().into())
+            },
+            "console" => {
+                Ok(io::stdout().into())
+            },
+            "file" => {
+                let file = File::create(self.get_or("output-file", "output.log"))?;
+                Ok(file.into())
+            },
+            "kafka" => {
+                let mut config = ClientConfig::new();
+                config
+                    .set("produce.offset.report", "true")
+                    .set("bootstrap.servers", &self.get_or("kafka-server", "localhost:9092"));
+                Ok(Drain::new(Box::new(kafkaesque::EventProducer::new(config, self.get_or("kafka-topic", "1")))))
+            },
+            _ => Err(Error::new(ErrorKind::Other, "Unknown output."))
+        }
     }
 }
