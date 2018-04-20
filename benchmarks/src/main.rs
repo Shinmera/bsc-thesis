d1ea6022a55386c124c3992efac283adf055887f
Shinmera <shinmera@tymoon.eu>
2018-04-20 15:12:40 +0200

    Clean up options.

--- benchmarks/src/main.rs
+++ benchmarks/src/main.rs
@@ -111,49 +111,87 @@ fn main() {
     }else if mode == "help" {
         eprintln!("Timely Benchmarks v0.1
 
-Usage: MODE [MODE-OPTIONS]
+Usage: MODE [OPTIONS]
 
-The following modes and respective options are available:
+The following modes are available:
 
-test                    Run the benchmarks.
-  --tests STRING          A comma-separated list of test names to run.
-  --threads NUM           Number of workers per process.
-  --process IDX           The identity of this process.
-  --processes NUM         Number of processes.
-  --hostfile FILE         Process address host file.
-  --report BOOL           Whether to report connection progress.
-  --window-size NUM       The size of the windows in epochs (usually seconds).
-  --window-slide NUM      The slide of the windows in epochs (usually seconds).
-  --input MODE            The input generation mode. Can be one of:
-                            null, console, file, generated
-  --output MODE           The output consuming mode. Can be one of:
-                            null, console, file, meter
+test                       Run the benchmarks.
+generate                   Generate data to files.
+help                       Show this help document.
 
-generate                Generate benchmark workloads.
-  --benchmarks STRING      A comma-separated list of benchmark names to generate.
+The following options are available:
+
+  --benchmarks STRING      A comma-separated list of benchmarks to run.
+                             Default: all
+  --tests STRING           A comma-separated list of test names to run.
+                             Default: all
+  --threads NUM            Number of workers per process.
+                             Default: 10
+  --process IDX            The identity of this process.
+                             Default: 0
+  --processes NUM          Number of processes.
+                             Default: 1
+  --hostfile FILE          Process address host file.
+                             Default: none
+  --report BOOL            Whether to report connection progress.
+                             Default: false
+  --window-size NUM        The size of the windows in epochs (usually seconds).
+                             Default: test-dependent
+  --window-slide NUM       The slide of the windows in epochs (usually seconds).
+                             Default: test-dependent
+  --input MODE             The input generation mode. Can be one of:
+                             null, console, file, generated
+                             Default: generated
+  --output MODE            The output consuming mode. Can be one of:
+                             null, console, file, meter
+                             Default: null
+  --log BOOL               Whether to log Timely events to a remote server.
+                             Default: false
+  --log-server HOST        The hostname of the server to connect to for logging.
+                             Default: localhost
+  --log-port PORT          The port of the server to connect to for logging.
+                             Default: 2102
   --data-dir DIR           The directory for the benchmark data files.
+                             Default: data/
   --threads NUM            The number of dataset partitions to create.
+                             Default: 10
+  --seconds NUM            How many seconds to generate events for.
+                             Default: 60
+  --events-per-second NUM  How many events to produce per second.
+                             Default: 100000
+  --ips NUM                (HIB) How many IPs to generate.
+                             Default: 100
   --campaigns NUM          (YSB) How many campaign IDs to generate.
+                             Default: 100
   --ads NUM                (YSB) How many ads per campaign to generate.
-  --seconds NUM            (YSB) How many seconds to generate events for.
-  --events-per-second NUM  (YSN) How many events to produce per second.
+                             Default: 10
   --rate-shape SINE/SQUARE (NEX) In what shape to generate delays between events.
-  --first-event-rate NUM   (NEX) 
+                             Default: SINE
   --next-event-rate NUM    (NEX)
-  --rate NUM               (NEX)
-  --event-generators NUM   (NEX)
+                             Default: events-per-second
+  --us-per-unit NUM        (NEX)
+                             Default: 1000000
   --rate-period NUM        (NEX)
+                             Default: 600
   --active-people NUM      (NEX)
+                             Default: 1000
   --in-flight-auctions NUM (NEX)
+                             Default: 100
   --out-of-order-group-size NUM (NEX)
+                             Default: 1
   --hot-seller-ratio NUM   (NEX)
+                             Default: 4
   --hot-auction-ratio NUM  (NEX)
+                             Default: 2
   --hot-bidder-ratio NUM   (NEX)
+                             Default: 2
   --first-event-id NUM     (NEX)
+                             Default: 0
   --first-event-number NUM (NEX)
+                             Default: 0
   --base-time NUM          (NEX)
+                             Default: 1436918400000 (2015-07-15T00:00:00.000Z)
 
-help                    Show this documentation.
 ");
     } else {
         eprintln!("Invalid mode, should be one of test, generate, help.");
