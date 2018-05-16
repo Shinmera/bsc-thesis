f9b902d676317f18495e83e62155a9dddebc759b
Shinmera <shinmera@tymoon.eu>
2018-05-16 16:49:15 +0200

    Add windowing results.

--- data/transform.lisp
+++ data/transform.lisp
@@ -17,6 +17,8 @@
                         "NEXMark Query 8" "NEXMark Query 9" "NEXMark Query 11"))
 (defparameter *rates* '(1000 10000 100000 1000000 10000000))
 (defparameter *workers* '(1 2 4 8 16 32))
+(defparameter *windows* '(20 40 60 80 120))
+(defparameter *slides* '(5 10 20 40 60))
 
 (defun read-file (file)
   (with-open-file (s file)
@@ -25,14 +27,17 @@
                             (list* name (sort (mapcar #'parse-float:parse-float (cl-ppcre:split " +" dat))
                                               #'<))))))
 
+(defun median (values)
+  (let ((sorted (sort values #'<)))
+    (nth (round (/ (length sorted) 2)) sorted)))
+
 (defun median-latency (&key (path "") (workers "32") (file (format NIL "latency-~a.csv" workers)))
   (with-open-file (out file :direction :output :if-exists :supersede)
     (dolist (rate *rates*)
       (with-simple-restart (continue "Ignore the error.")
         (let ((group (read-file (merge-pathnames (format NIL "~a@~a.csv" rate workers) path))))
           (format out "~&~a~{ ~f~}" rate (loop for (k . v) in group
-                                               for sorted = (sort v #'<)
-                                               collect (nth (round (/ (length sorted) 2)) sorted))))))))
+                                               collect (median v))))))))
 
 (defun scaling (&key (path "") (rate "10000000") (file (format NIL "scaling-~a.csv" rate)))
   (with-open-file (out file :direction :output :if-exists :supersede)
@@ -40,8 +45,7 @@
       (with-simple-restart (continue "Ignore the error.")
         (let ((group (read-file (merge-pathnames (format NIL "~a@~a.csv" rate workers) path))))
           (format out "~&~a~{ ~f~}" workers (loop for (k . v) in group
-                                                  for sorted = (sort v #'<)
-                                                  collect (nth (round (/ (length sorted) 2)) sorted))))))))
+                                                  collect (median v))))))))
 
 (defun cdf (&key (path "") (workers "32") (rate "10000000") (file (format NIL "cdf-~a-~a.csv" workers rate)))
   (with-open-file (out file :direction :output :if-exists :supersede)
@@ -52,6 +56,28 @@
                (loop for cons in group
                      do (format out " ~f" (pop (cdr cons))))))))
 
+(defun window (&key (path "") (rate "10000000") (workers "32") (file (format NIL "window-~a-~a.csv" workers rate)))
+  (with-open-file (out file :direction :output :if-exists :supersede)
+    (dolist (window *windows*)
+      (with-simple-restart (continue "Ignore the error.")
+        (let ((group (read-file (merge-pathnames (format NIL "~a@~a-w~a.csv" rate workers window) path))))
+          (format out "~&~a~{ ~f~}" window
+                  (loop for (k . v) in group
+                        collect (median v))))))))
+
+(defun slide (&key (path "") (rate "10000000") (workers "32") (file (format NIL "slide-~a-~a.csv" workers rate)))
+  (with-open-file (out file :direction :output :if-exists :supersede)
+    (dolist (window *windows*)
+      (with-simple-restart (continue "Ignore the error.")
+        (format out "~&~a" window)
+        (dolist (slide *slides*)
+          (handler-case
+              (let ((group (read-file (merge-pathnames (format NIL "~a@~a-w~as~a.csv" rate workers window slide) path))))
+                (format out " ~f" (or (median (cdr (first group))) "nan")))
+            (error (err)
+              (declare (ignore err))
+              (format out " nan"))))))))
+
 (defun sync ()
   (uiop:run-program (list "rsync" "-avz" "hafnern@sgs-r815-03:/mnt/local/hafnern/thesis/benchmarks/*.csv" ".") :output T :error-output T))
 
@@ -61,7 +87,9 @@
   (median-latency :workers 16)
   (median-latency :workers 32)
   (scaling)
-  (cdf))
+  (cdf)
+  (window)
+  (slide))
 
 (defun toplevel (&rest args)
   (let ((kargs ()))
